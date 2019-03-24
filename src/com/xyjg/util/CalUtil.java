2b220d32ebddcf9a80bc81f3590fd63ca5122ed7
xiaoyujunge <952659044@qq.com>
2019-03-24 22:30:43 +0800

    计算工具类

---
+++ src/com/xyjg/util/CalUtil.java
@@ -0,0 +1,21 @@
+package com.xyjg.util;
+
+/**
+ * @ClassName CalUtil
+ * @Description 计算工具类
+ * @Author Zhoucj
+ * @Date 2019-3-24
+ **/
+public class CalUtil {
+    /**
+     * 数组中两个数进行交换
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap(int array[], int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
